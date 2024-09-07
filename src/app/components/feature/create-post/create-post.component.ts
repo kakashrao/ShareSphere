449e1ffbf49c3448a6bc0aa59138d2ff34a51333
kakashrao <kakashrao.2021@gmail.com>
2024-09-07 13:45:45 +0530

    added link adding functioanality to editor

--- src/app/components/feature/create-post/create-post.component.ts
+++ src/app/components/feature/create-post/create-post.component.ts
@@ -1,24 +1,52 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { Component, ViewChild, ViewEncapsulation } from "@angular/core";
 import { ButtonModule } from "primeng/button";
-import { EditorModule } from "primeng/editor";
+import { ConfirmPopupModule } from "primeng/confirmpopup";
+import { Editor, EditorModule } from "primeng/editor";
+import { FileUploadModule } from "primeng/fileupload";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputGroupAddonModule } from "primeng/inputgroupaddon";
+import { InputTextModule } from "primeng/inputtext";
+import { OverlayPanel, OverlayPanelModule } from "primeng/overlaypanel";
+import { RippleModule } from "primeng/ripple";
+import { Range } from "quill";
 
 @Component({
   selector: "sp-create-post",
   standalone: true,
-  imports: [EditorModule, ButtonModule, ReactiveFormsModule],
+  imports: [
+    EditorModule,
+    ButtonModule,
+    FileUploadModule,
+    ConfirmPopupModule,
+    OverlayPanelModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    RippleModule,
+  ],
+  providers: [],
   templateUrl: "./create-post.component.html",
   styleUrl: "./create-post.component.scss",
   encapsulation: ViewEncapsulation.None,
 })
 export class CreatePostComponent {
-  editorControl = new FormControl<string>("");
+  @ViewChild("postEditor") editor!: Editor;
 
-  constructor() {
-    this.editorControl.valueChanges.subscribe({
-      next: (value: string | null) => {
-        console.log(value);
-      },
+  openLinkOverlay(panel: OverlayPanel, event: MouseEvent) {
+    const range = this.editor.quill.history.currentRange as Range;
+
+    if (!range?.length) return;
+    panel.toggle(event);
+  }
+
+  onAddLink(linkInput: HTMLInputElement, panel: OverlayPanel) {
+    const range = this.editor.quill.history.currentRange as Range;
+
+    this.editor.quill.formatText(range.index, range.length, {
+      link: linkInput.value,
     });
+
+    linkInput.value = "";
+    panel.hide();
   }
 }
